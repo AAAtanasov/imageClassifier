c059490dc40af3f3b75047a77dffe976390c68a6
Anton Atanasov <anton.atanasov@amexis.net>
2017-05-11 10:25:57 +0200

    refined cross validation

--- load.py
+++ load.py
@@ -4,6 +4,7 @@ import numpy as np
 import os.path
 import random
 import math
+import matplotlib.pyplot as plt
 from itertools import repeat
 
 """
@@ -16,7 +17,7 @@ def iterate_class_folders(number_of_classes):
     main_dir = "101_ObjectCategories"
     sub_folders_list = []
     all_folder_names = os.listdir(main_dir)
-    all_folder_names.remove("BACKGROUND_Google")
+    # all_folder_names.remove("BACKGROUND_Google")
 
     for tempIndex, _ in enumerate(range(number_of_classes)):
         choice_item_index = random.randrange(len(all_folder_names))
@@ -45,7 +46,7 @@ def retrieve_image_from_folder(folder_name):
 
     def random_index_assign(incoming_list, count_of_elems_to_extract, global_ten_fold_array, array_index):
         items_to_add = extract_from_array(incoming_list, count_of_elems_to_extract)
-        global_ten_fold_array[array_index].extend(items_to_add)
+        global_ten_fold_array[array_index].extend(items_to_add)  # Extending global list containing files
         return
 
     def extract_from_array(incoming_list, number_to_extract):
@@ -58,53 +59,36 @@ def retrieve_image_from_folder(folder_name):
 
         return result_items
 
-    length_of_images = len(list_of_images)
-    # uneven_elements_count = len(list_of_images) % 10
-    # rounded_elements_count = len(list_of_images) - uneven_elements_count
-    # ten_percent_extractable_integer = length_of_images / 10
+    length_of_images = len(list_of_images)  # Number of images in folder
+    real_element_division = length_of_images / 10  # Iterator
+    real_index = 0  # Temporal value to store the current step state
+    last_start_position = 0  # Temporal value to store the last addition value
 
-    real_element_division = length_of_images / 10
-    real_index = 0
-    last_start_position = 0
-
-    for elementIndex, value in enumerate(range(10)):
-        real_index += real_element_division
-        temp_index = math.floor(real_index - last_start_position)
+    for elementIndex, _ in enumerate(range(10)):  # Enumerate over 10-fold list
+        real_index += real_element_division  # Real valued index kept for state
+        temp_index = math.floor(real_index - last_start_position)  # Integer value
         last_start_position += temp_index
 
         if last_start_position != length_of_images and elementIndex == 9:
             temp_index += length_of_images - last_start_position
-        # if real_index == real_element_division:
-        #     temp_index = math.floor(real_index)
-        # elif real_index != length_of_images:
-        #     temp_index = math.floor(real_index - real_element_division * elementIndex + 1)
-        # else:
-        #     temp_index = math.ceil(real_index - real_element_division * 9)
 
         random_index_assign(list_of_images, temp_index,
                             ten_fold_array, elementIndex)
 
 
-
-        # if (index + 1) > uneven_elements_count:
-        #     random_index_assign(list_of_images, ten_percent_extractable_integer,
-        #                         ten_fold_array, elementIndex)
-        # else:
-        #     random_index_assign(list_of_images, ten_percent_extractable_integer + 1,
-        #                         ten_fold_array, elementIndex)
-        # index += 1
-
-
-iterate_class_folders(100)
+iterate_class_folders(101)
 print(ten_fold_array)
 # temp_image = cv2.imread(ten_fold_array[0][0])
 
 """Corner detection"""
+
+
 def corner_detection(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = np.float32(gray)
 
     corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
+    cv2.goodFeaturesToTrack()
     corners = np.int0(corners)
 
     for corner in corners:
@@ -113,24 +97,40 @@ def corner_detection(image):
 
     cv2.imshow("temp", image)
 
+
 def edge_detection(image):
     # edges = cv2.Laplacian(image, cv2.CV_64F)    #cv2.Canny(image, 100, 100)
     edges = cv2.Canny(image, 100, 100)
 
     cv2.imshow("Canny", edges)
-#
-# for partial in ten_fold_array[0]:
-#     temp_image = cv2.imread(partial)
-#     edge_detection(temp_image)
-# edge_detection(temp_image)
+
+
+def brute_force(image1, image2):
+    orb = cv2.ORB_create()
+
+    kp1, des1 = orb.detectAndCompute(image1, None)
+    kp2, des2 = orb.detectAndCompute(image2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    image3 = cv2.drawMatches(image1, kp1, image2, kp2, matches[:20], None, flags=2)
+    plt.imshow(image3)
+    plt.show()
+
 
 temp_image = cv2.imread(ten_fold_array[0][0])
+temp_image1 = cv2.imread(ten_fold_array[0][1])
 # temp_image1 = cv2.imread(ten_fold_array[0][1])
 # temp_image2 = cv2.imread(ten_fold_array[0][2])
 # temp_image3 = cv2.imread(ten_fold_array[0][3])
 
 edge_detection(temp_image)
 # edge_detection(temp_image1)
+# corner_detection(temp_image)
 
+# brute_force(temp_image, temp_image1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
